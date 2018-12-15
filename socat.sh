e99ff7fd790be9ad903cdbedf5724852af9727ff
Grm <grm.klein@gmail.com>
2018-12-15 16:27:05 +0100

    correcting bash script

--- socat.sh
+++ socat.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-CMD=socat -d -d -d -d pty,raw,echo=0,link=${LINK_FILE} tcp-connect:${REMOTE_SERVER}:${REMOTE_PORT}
-echo "Running : "
-echo "$CMD"
-
-$(CMD)
+socat -d -d -d -d pty,raw,echo=0,link=${LINK_FILE} tcp-connect:${REMOTE_SERVER}:${REMOTE_PORT}
